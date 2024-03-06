138041f6925ad96354fde527f6ddf1c18b79cc53
Your Name <you@example.com>
2024-03-06 10:15:26 +0200

    pok

--- main.py
+++ main.py
@@ -36,15 +36,45 @@ while True:
     choice = input("Enter your choice (1-5): ")
 
     if choice == '1':
+        x = int(input("Write pokemon index: "))
+        x = x - 1
+        print(pokemons[x])
+        print(x)
         # https://www.w3schools.com/python/python_dictionaries_access.asp
         pass
     elif choice == '2':
+        def sort_total(pokemon):
+            return int(pokemon["total"])
+        pokemons.sort(key = sort_total, reverse = True)
+        print(pokemons[:11])
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '3':
+        def sort_total(pokemon):
+            return int(pokemon["total"])
+        pokemons.sort(key = sort_total, reverse = False) 
+        print(pokemons[:11])
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '4':
+        x = int(input("Write pokemon index: "))
+        x = x - 1
+        My_choose = pokemons[x]
+        print("Player 1(your) pokemon: ", My_choose)
+        
+
+        Computer_choose = random.choice(pokemons)
+        print("Player 2 pokemon: ", Computer_choose)
+
+        player_1_damage = My_choose["attack"] - Computer_choose["defense"] + random.randint(5, 20)
+        player_2_damage = Computer_choose["attack"] - My_choose["defense"] + random.randint(5, 20)
+        print("Player 1 damage player 2:", player_1_damage, "points.")
+        print("Player 2 damage player 1:", player_2_damage, "points.")
+        
+
+
+
+
         # Battle
         # 
         # https://www.w3schools.com/python/ref_random_choice.asp - random choice
